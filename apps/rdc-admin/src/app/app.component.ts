abd21edb89560e927cd27f148ebcc4108af7450e
TomBickle <tom.bickle@rdcaviation.com>
2022-09-09 09:47:09 +0100

    Initial commit

---
+++ apps/rdc-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rdc-final-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rdc-admin';
+}
